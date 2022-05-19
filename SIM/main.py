02748067e288fcab34dfdd40ebc124864ff8e6a0
Pedro Abreu <pedro.abreu@ua.pt>
2022-05-19 08:30:35 +0100

    add 3 obu, sendings cam in broadcast

--- SIM/main.py
+++ SIM/main.py
@@ -1,40 +1,56 @@
+from concurrent.futures import thread
 import paho.mqtt.client as mqtt
 from threading import Thread
 import json
 import time
 import os
 
+def update_cam(cam, update):
+    for key in update.keys():
+        if key in cam.keys():
+            cam[key] = update[key]
+        else:
+            print("update CAM with bad configuration!")
+
+    return cam
+
+def on_message(client, userdata, message):
+    print("received message latitude=",str(json.loads(message.payload.decode("utf-8"))['latitude']))
 
 def launch_obu(data):
-    client = mqtt.Client()
+    client = mqtt.Client(client_id=f'_{data[0]}')
     client.connect(data[0]['ip'], 1883, 60)
-    client.publish("vanetza/in/cam", json.dumps(data[1]))
+    client.on_message = on_message
+    cam = data[1]
+    client.publish("vanetza/in/cam", json.dumps(cam))
+    client.subscribe('vanetza/out/cam')
+    count = 0.5
 
     while True:
-        
-        ## update cam info with data[3] dict
-
-
-
+    ## update cam
+        if str(count) in data[2].keys():
+            cam = update_cam(cam, data[2][str(count)])
+        client.publish("vanetza/in/cam", json.dumps(cam))
+        count += 0.5
         time.sleep(0.5)
+        client.loop() #check for messages
 
+    time.sleep(2) # wait
     client.disconnect()
 
 def main():
     files = os.listdir('obu')
     files = [f.split('.')[0] for f in files]
 
-    threads = [Thread(target=launch_obu, name=f'_{obu}', args=(json.load(open(f'obu/{obu}.json')), )) for obu in files]
+    threads = [Thread(target=launch_obu, name=f'_{obu}', args=(json.load(open(f'obu/{obu}.json')),  )) for obu in files]
 
-    # start the threads
-    for thread in threads:
-        print(f'thread: {thread.name} started!\n')
-        thread.start()
+    ## start the threads
+    print(f'threads started!\n')
+    [t.start() for t in threads]
 
-    # wait for the threads to complete
-    for thread in threads:
-        thread.join()
-        print(f'thread: {thread.name} finished!\n')
+    ## stop the threads
+    [t.join() for t in threads]
+    print(f'threads finished!\n')
 
     return 0
 
